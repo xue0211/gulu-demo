b084952b157a7a4d41885676b02d16e3c2ab50de
xue0211 <2723709118@qq.com>
2024-10-08 22:03:45 +0800

    添加 click 事件

--- dist/app.a6a4d504.js
+++ dist/app.a6a4d504.js
@@ -9838,10 +9838,15 @@ exports.default = void 0;
 //
 //
 //
+//
 var _default = exports.default = {
   //props: ['icon', 'iconPosition']
   props: {
     icon: {},
+    loading: {
+      type: Boolean,
+      default: false
+    },
     iconPosition: {
       type: String,
       default: 'left',
@@ -9868,14 +9873,24 @@ var _default = exports.default = {
     "button",
     {
       staticClass: "g-button",
-      class: ((_obj = {}), (_obj["icon-" + _vm.iconPosition] = true), _obj)
+      class: ((_obj = {}), (_obj["icon-" + _vm.iconPosition] = true), _obj),
+      on: {
+        click: function($event) {
+          _vm.$emit("click")
+        }
+      }
     },
     [
-      _vm.icon
+      _vm.icon && !_vm.loading
         ? _c("g-icon", { staticClass: "icon", attrs: { name: _vm.icon } })
         : _vm._e(),
       _vm._v(" "),
-      _c("g-icon", { staticClass: "loading", attrs: { name: "loading" } }),
+      _vm.loading
+        ? _c("g-icon", {
+            staticClass: "loading icon",
+            attrs: { name: "loading" }
+          })
+        : _vm._e(),
       _vm._v(" "),
       _c("div", { staticClass: "content" }, [_vm._t("default")], 2)
     ],
@@ -9989,7 +10004,12 @@ function _interopRequireDefault(e) { return e && e.__esModule ? e : { default: e
 _vue.default.component('g-button', _button.default);
 _vue.default.component('g-icon', _icon.default);
 new _vue.default({
-  el: '#app'
+  el: '#app',
+  data: {
+    loading1: false,
+    loading2: true,
+    loading3: false
+  }
 });
 },{"vue":"node_modules/vue/dist/vue.common.js","./button":"src/button.vue","./icon":"src/icon.vue"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
