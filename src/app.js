a42167d7ef0315da9c73511ae700d97620effb01
xue0211 <2723709118@qq.com>
2024-10-09 10:36:21 +0800

    使用 chai.spy 监听回调函数

--- src/app.js
+++ src/app.js
@@ -17,6 +17,8 @@ new Vue({
 })
 
 import chai from 'chai';
+import spies from 'chai-spies';
+chai.use(spies)
 
 const expect = chai.expect
 
@@ -94,10 +96,11 @@ const expect = chai.expect
         }
     })
     vm.$mount()
-    vm.$on('click', function(){
-       expect(1).to.eq(1)
-    })
+    let spy = chai.spy(function(){})
+
+    vm.$on('click', spy)
     let button = vm.$el
     button.click()
+    expect(spy).to.have.been.called()
 
 }
