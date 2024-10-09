feaf461e0b978f70ccfabb52e0e56e2a220d9abe
xue0211 <2723709118@qq.com>
2024-10-09 15:26:49 +0800

    使用 npx test 和 npx dev-test 自动测试

--- src/button.vue
+++ src/button.vue
@@ -1,26 +1,29 @@
 <template>
-    <button class="g-button" :class="{[`icon-${iconPosition}`]: true}"
-    @click="$emit('click')">
-      <g-icon class="icon" v-if="icon && !loading" :name="icon"></g-icon>
-      <g-icon class="loading icon" v-if="loading" name="loading"></g-icon>
-      <div class="content">
-        <slot></slot>
-      </div>
-    </button>
+  <button class="g-button" :class="{[`icon-${iconPosition}`]: true}"
+          @click="$emit('click')">
+    <g-icon class="icon" v-if="icon && !loading" :name="icon"></g-icon>
+    <g-icon class="loading icon" v-if="loading" name="loading"></g-icon>
+    <div class="content">
+      <slot></slot>
+    </div>
+  </button>
 </template>
 <script>
+import Icon from './icon'
 export default {
-  //props: ['icon', 'iconPosition']
+  components: {
+    'g-icon': Icon
+  },
   props: {
     icon: {},
-    loading:{
-      type:Boolean,
+    loading: {
+      type: Boolean,
       default: false
     },
     iconPosition: {
       type: String,
       default: 'left',
-      validator(value){
+      validator(value) {
         return value === 'left' || value === 'right'
       }
     }
@@ -29,25 +32,61 @@ export default {
 </script>
 <style lang="scss">
 @keyframes spin {
-  0%{ transform: rotate(0deg); }
-  100%{ transform: rotate(360deg); }
-}
-.g-button { font-size: var(--font-size); height: var(--button-height); padding: 0 1em;
-  border-radius: var(--border-radius); border: 1px solid var(--border-color);
-  background: var(--button-bg);
-  display: inline-flex; vertical-align: middle;
-  justify-content: center;align-items: center;
-  &:hover { border-color: var(--border-hover-color);}
-  &:active { background-color: var(--button-active-bg);}
-  &:focus { outline: none;}
-  > .content {order: 2;}
-  > .icon {order: 1; margin-right: .1em;}
-  &.icon-right {
-    > .content {order: 1;}
-    > .icon {order: 2; margin-right: 0; margin-left: .1em;}
+  0% {
+    transform: rotate(0deg);
   }
-  .loading{
-    animation: spin 1.2s infinite linear; ;
+  100% {
+    transform: rotate(360deg);
+  }
+}
+
+.g-button {
+  font-size: var(--font-size);
+  height: var(--button-height);
+  padding: 0 1em;
+  border-radius: var(--border-radius);
+  border: 1px solid var(--border-color);
+  background: var(--button-bg);
+  display: inline-flex;
+  vertical-align: middle;
+  justify-content: center;
+  align-items: center;
+
+  &:hover {
+    border-color: var(--border-hover-color);
+  }
+
+  &:active {
+    background-color: var(--button-active-bg);
+  }
+
+  &:focus {
+    outline: none;
+  }
+
+  > .content {
+    order: 2;
+  }
+
+  > .icon {
+    order: 1;
+    margin-right: .1em;
+  }
+
+  &.icon-right {
+    > .content {
+      order: 1;
+    }
+
+    > .icon {
+      order: 2;
+      margin-right: 0;
+      margin-left: .1em;
+    }
+  }
+
+  .loading {
+    animation: spin 1.2s infinite linear;;
   }
 }
 </style>/
