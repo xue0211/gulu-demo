926149045dd1f00cfded0c67e7b885f8c2ed2f98
xue0211 <2723709118@qq.com>
2024-10-14 11:27:39 +0800

    稍微重构了一下

--- dist/app.a6a4d504.js
+++ dist/app.a6a4d504.js
@@ -10409,6 +10409,18 @@ var _default = exports.default = {
       gutter: 0
     };
   },
+  methods: {
+    createClasses: function createClasses(obj) {
+      var str = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : '';
+      if (!obj) {
+        return [];
+      }
+      var array = [];
+      if (obj.span) array.push("col-".concat(str).concat(obj.span));
+      if (obj.offset) array.push("offset-".concat(str).concat(obj.offset));
+      return array;
+    }
+  },
   computed: {
     colClass: function colClass() {
       var span = this.span,
@@ -10417,7 +10429,11 @@ var _default = exports.default = {
         narrowPc = this.narrowPc,
         pc = this.pc,
         widePc = this.widePc;
-      return [span && "col-".concat(span), offset && "offset-".concat(offset)].concat(_toConsumableArray(ipad ? ["col-ipad-".concat(ipad.span)] : []), _toConsumableArray(narrowPc ? ["col-narrow-pc-".concat(narrowPc.span)] : []), _toConsumableArray(pc ? ["col-pc-".concat(pc.span)] : []), _toConsumableArray(widePc ? ["col-wide-pc-".concat(widePc.span)] : []));
+      var createClasses = this.createClasses;
+      return [].concat(_toConsumableArray(createClasses({
+        span: span,
+        offset: offset
+      })), _toConsumableArray(createClasses(ipad, 'ipad-')), _toConsumableArray(createClasses(narrowPc, 'narrow-pc-')), _toConsumableArray(createClasses(pc, 'pc-')), _toConsumableArray(createClasses(widePc, 'wide-pc-')));
     },
     colStyle: function colStyle() {
       return {
