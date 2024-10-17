971223ee0ceaf62e11bd0aeb99f802597d37f80b
xue0211 <2723709118@qq.com>
2024-10-17 11:08:06 +0800

    tab 切换动画完成

--- src/tabs-head.vue
+++ src/tabs-head.vue
@@ -11,10 +11,11 @@
 export default {
   name: 'GuluTabsHead',
   inject: ['eventBus'],
-  created() {
+  mounted() {
     this.eventBus.$on('update:selected',(item, vm)=>{
-      console.log(item);
-      console.log(vm.$el.getBoundingClientRect());
+      let {width, height, top, left} = vm.$el.getBoundingClientRect();
+      this.$refs.line.style.width = `${width}px`;
+      this.$refs.line.style.left = `${left}px`;
     })
   }
 }
@@ -26,13 +27,12 @@ $blue: blue;
   display: flex;
   height: $tab-height;
   justify-content: flex-start;
-  border: 1px solid red;
   position: relative;
   > .line{
     position: absolute;
     bottom: 0;
     border-bottom:1px solid $blue;
-    width: 100px;
+    transition: all 250ms;
   }
   > .actions-wrapper{
     margin-left: auto;
