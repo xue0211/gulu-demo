27d915f752ba09ee25f7e2ba41730e81f98020f0
xue0211 <zhengtai1123.qq.com>
2024-10-20 18:48:11 +0800

    创建官网

--- src/button.vue
+++ src/button.vue
@@ -31,61 +31,34 @@ export default {
   }
 }
 </script>
-<style lang="scss">
+<style lang="scss" scoped>
+$font-size: 14px;
+$button-height: 32px;
+$button-bg: white;
+$button-active-bg: #eee;
+$border-radius: 4px;
+$color: #333;
+$border-color: #999;
+$border-color-hover: #666;
 @keyframes spin {
-  0% {
-    transform: rotate(0deg);
-  }
-  100% {
-    transform: rotate(360deg);
-  }
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
 }
-
-.g-button {
-  font-size: var(--font-size);
-  height: var(--button-height);
-  padding: 0 1em;
-  border-radius: var(--border-radius);
-  border: 1px solid var(--border-color);
-  background: var(--button-bg);
-  display: inline-flex;
-  vertical-align: middle;
-  justify-content: center;
+.g-button { font-size: $font-size; height: $button-height; padding: 0 1em;
+  border-radius: $border-radius; border: 1px solid $border-color;
+  background: $button-bg;
+  display: inline-flex; vertical-align: middle; justify-content: center;
   align-items: center;
-
-  &:hover {
-    border-color: var(--border-hover-color);
-  }
-
-  &:active {
-    background-color: var(--button-active-bg);
-  }
-
-  &:focus {
-    outline: none;
-  }
-
-  > .content {
-    order: 2;
-  }
-
-  > .icon {
-    order: 1;
-    margin-right: .1em;
-  }
+  &:hover { border-color: $border-color-hover; }
+  &:active { background-color: $button-active-bg; }
+  &:focus { outline: none; }
+  > .content { order: 2; }
+  > .icon { order: 1; margin-right: .1em; }
 
   &.icon-right {
-    > .content {
-      order: 1;
-    }
-
-    > .icon {
-      order: 2;
-      margin-right: 0;
-      margin-left: .1em;
-    }
+    > .content { order: 1; }
+    > .icon { order: 2; margin-right: 0; margin-left: .1em; }
   }
-
   .loading {
     animation: spin 1.2s infinite linear;;
   }
